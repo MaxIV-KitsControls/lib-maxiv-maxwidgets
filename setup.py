ec4f0f7b82e92d7a1fc88d8b28263f2bde5aa92b
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2014-06-12 18:34:49 +0200

    v0.8.3

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class install_data(_install_data):
         _install_data.run(self)
 
 setup(name = 'python-maxwidgets',
-      version = '0.8.2',
+      version = '0.8.3',
       description = 'A collection of reusable Taurus widgets',
       packages = packages,
       package_dir = {'maxwidgets' : 'src'},
