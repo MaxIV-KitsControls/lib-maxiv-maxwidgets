0dd55bf4b359a2068441fe96d104f8ce690e4e35
Johan Forsberg <johan.forsberg@maxlab.lu.se>
2015-09-18 16:58:11 +0200

    bump

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ class install_data(_install_data):
         _install_data.run(self)
 
 setup(name = 'python-maxwidgets',
-      version = '0.8.7',
+      version = '0.8.8',
       description = 'A collection of reusable Taurus widgets',
       packages = packages,
       package_dir = {'maxwidgets' : 'src'},
